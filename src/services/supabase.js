721beef3b7454d2e756691bd1099836e249f2cea
Kemmy112 <159628410+Kemmy112@users.noreply.github.com>
2025-06-26 21:42:28 -0700

    major changes made

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,9 @@
 // supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hcohhbpqnjhjnnjzppxg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhjb2hoYnBxbmpoam5uanpwcHhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5MzU3MTUsImV4cCI6MjA2NjUxMTcxNX0.hn0RMaqmbvDL8ZLQ4b8XThLlQ8G3XHGd3mVJwcIO8lc';
+const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL ,
+    process.env.REACT_APP_SUPABASE_ANON_KEY
+);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default supabase;
