44e410690133cc82957a0f87b3ce04f60af37c44
Kemmy112 <159628410+Kemmy112@users.noreply.github.com>
2025-09-02 08:57:23 -0700

    just ran npm run build to prepare the project for Vercel deployment

--- src/app.js
+++ src/app.js
@@ -3,14 +3,11 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './styles/index.css';
 import './styles/app.css';
 import 'boxicons/css/boxicons.css';
-// import 'boxicons/dist/boxicons.js';
 import Navbar from './components/navbar';
 import Homepage from './pages/home';
-import Onboarding from './pages/onboardingpage';
 import Dashboard from './pages/dashboard';
 import About from './pages/about';
 import Footer from './components/footer';
-import Timer from './pages/timer';
 
 function App() {
   return (
@@ -18,10 +15,8 @@ function App() {
     <Routes>
       <Route path='/' element={<Homepage/>} />
       <Route path='/navbar' element={<Navbar/>} />
-      <Route path='/onboarding' element={<Onboarding/>} />
       <Route path='/dashboard' element={<Dashboard/>} />
       <Route path='/about' element={<About/>} />
-      <Route path='/timer' element={<Timer/>} />
       <Route path='/footer' element={<Footer/>} />
     </Routes>
    </Router>
